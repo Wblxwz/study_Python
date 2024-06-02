798558c58bd8839bef835875959557da31ed3b1e
wblxwz <2394559659@qq.com>
2024-06-02 22:18:44 +0800

    python 8 note

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 
+import copy
+import weakref
+
 def deco(func):
     """装饰器"""
     j = 10
@@ -14,5 +17,24 @@ def deco(func):
 def ceShi(a):
     return a
 
+def cs(s):
+    print(id(s))
+    s.append(4)
+    print(id(s))
+    return s
+
+class BB:
+    a = None
+
+class AA:
+    b = BB
+
+def bye():
+    print('Bye~')
 if (__name__ == '__main__'):
-    print(ceShi(5))
+    a = AA
+    b = BB
+    b.a = a
+    weak_a = weakref.finalize(a,bye)
+    weak_b = weakref.finalize(b,bye)
+    print()
