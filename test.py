87907a643645e9a3be43474f5b42d6322cb1386f
wblxwz <2394559659@qq.com>
2024-06-02 15:16:15 +0800

    init

---
+++ test.py
@@ -0,0 +1,16 @@
+
+def deco(func):
+    """装饰器"""
+    j = 10
+    print("deco...")
+    def deco2(*args):
+        nonlocal j
+        j += 5
+        print(args,j)
+        return func(*args)
+    return deco2
+
+@deco
+def ceShi(a):
+    return a
+
