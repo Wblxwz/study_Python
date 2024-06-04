2dc2ae29e4d3831d55105c7530b964240e81b221
wblxwz <2394559659@qq.com>
2024-06-04 18:34:17 +0800

    add 13

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-
+import array
 import copy
 import weakref
+import struct
+import abc
+from functools import singledispatch
 
 def deco(func):
     """装饰器"""
@@ -14,7 +17,7 @@ def deco(func):
     return deco2
 
 @deco
-def ceShi(a):
+def ceShi(a,b):
     return a
 
 def cs(s):
@@ -23,18 +26,86 @@ def cs(s):
     print(id(s))
     return s
 
-class BB:
-    a = None
+class MyTuple:
 
-class AA:
-    b = BB
+    __pri = 10
+    __att = 20
+    @property
+    def pri(self):
+        return self.__pri
+
+    def __getattr__(self, item):
+        print(item)
+        return self.__att
+
+    def __add__(self, other):
+        return MyTuple(self.x + other.x,self.y + other.y)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __iter__(self):
+        return (i for i in (self.x,self.y))
+    def __repr__(self):
+        return '{!r},{!r}'.format(*self)
+    def __bytes__(self):
+        return bytes([ord('i')]) + bytes(array.array('i',self))
+    #__slots__ = ('x','y')
+    def __del__(self):
+        print('del...')
+    def __len__(self):
+        return 2
+    def __format__(self, format_spec):
+        return '{!r},{!r}'.format(self.x,self.y)
+    @classmethod
+    def ces(*args):
+        print(args)
 
 def bye():
     print('Bye~')
+class Parent(abc.ABC):
+    @abc.abstractmethod
+    def a(self):
+        pass
+    def b(self):
+        return 100
+class Son(Parent):
+    def a(self):
+        return 1
+    def __init__(self):
+        self.s = '1'
+    @staticmethod
+    def c():
+        return 11
+@singledispatch
+def single(a):
+    print(a)
+@single.register(int)
+def single1(a):
+    print('int',a)
+@single.register(str)
+def single1(a):
+    print('str',a)
 if (__name__ == '__main__'):
-    a = AA
-    b = BB
-    b.a = a
-    weak_a = weakref.finalize(a,bye)
-    weak_b = weakref.finalize(b,bye)
-    print()
+    myTuple = MyTuple(0,1)
+    # print(struct.pack('<I',1))
+    # print(bytes(myTuple))
+    s = '{name:0.2f}'
+    print(s.format(name=0.44444))
+
+    list1 = [1,2,3,4,5]
+    t = type(myTuple)
+    tt = t(2,3)
+    print(tt.__att)
+    tt.abc = 55
+    #Parent.register(Son)
+    print(issubclass(Son,Parent))
+    son = Son()
+    print(Son.a(son))
+    print(Son.__mro__)
+    print(myTuple + tt)
+    single(1)
+    single('123')
