50514e6cfe336f0103d3c04734fe13eb00c4d19f
wblxwz <2394559659@qq.com>
2024-06-27 23:30:58 +0800

    selenium

--- SeleniumStudy.py
+++ SeleniumStudy.py
@@ -1,17 +1,23 @@
 import time
 from test import test_print
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 test_print()
 driver = webdriver.ChromiumEdge()
 
-driver.get("https://www.baidu.com")
+driver.maximize_window()
+driver.get("https://www.bilibili.com")
 
-kw = driver.find_element("id","kw")
+kw = driver.find_element(By.CLASS_NAME,"nav-search-input")
 kw.send_keys("cs")
-submit = driver.find_element("id","su")
+submit = driver.find_element("class name","nav-search-btn")
 submit.click()
 
+driver.back()
+driver.forward()
+
+driver.refresh()
 time.sleep(5)
 
 print("%r" % "quit")
